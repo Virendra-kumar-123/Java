ea95a703f4e6e4fd03aa1489b8fb0a60147e0f95
Virendra-kumar-123 <71625506+virendra-kumar-123@users.noreply.github.com>
2025-08-13 00:09:12 +0530

    Create README.md

--- src/Java/i_DeclarationAndAccessControl.java
+++ src/Java/i_DeclarationAndAccessControl.java
@@ -1,6 +1,6 @@
 package Java;
 
-public class DeclarationAndAccessControl {
+public class i_DeclarationAndAccessControl {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
